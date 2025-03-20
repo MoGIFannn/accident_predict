b18010aedb26acf022601b69a7056b3dac7e11dc
MoGIFannn <161975278+MoGIFannn@users.noreply.github.com>
2025-03-20 01:13:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def show_map():
     return render_template('predicted_map.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
